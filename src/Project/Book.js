ea4a95881f4467eb4f1f8cfc7a6930144bc9ff58
Sridhar2601 <sridharchml@gmail.com>
2025-09-24 14:12:57 +0530

    all

--- src/Project/Book.js
+++ src/Project/Book.js
@@ -1,9 +1,75 @@
-import './CSS/Home.css'
+// import './CSS/Home.css'
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// export default function Book() {
+
+//     const [data, setdata] = useState({
+//         Name: "",
+//         Ph_Number: "",
+//         Email: "",
+//         Person: "",
+//         Date: ""
+//     });
+
+//     const form = (e) => {
+//         setdata({
+//             ...data,
+//             [e.target.name]: e.target.value
+//         });
+//     };
+
+//     const submit = (e) => {
+//         e.preventDefault();
+//         axios.post("https://6888a259adf0e59551bac01f.mockapi.io/api/intern/Contact", data)
+
+//         setdata({
+//             Name: "",
+//             Ph_Number: "",
+//             Email: "",
+//             Person: "",
+//             Date: ""
+//         })
+//     }
+//     return (
+//         <div>
+//             <div className='container pt-5 mt-5 pb-5'>
+//                 <h1>Book your Table</h1>
+//                 <div className='row pt-4'>
+//                     <div className='col-lg-6'>
+//                         <form onSubmit={submit}>
+//                             <input type="text" placeholder='Your Name' name="Name" value={data.Name} onChange={form} style={{ width: '500px', height: '50px', borderRadius: '10px' }}></input><br></br><br></br>
+//                             <input type="number" placeholder='Phone Number' name='Ph_Number' value={data.Ph_Number} onChange={form} style={{ width: '500px', height: '50px', borderRadius: '10px' }}></input><br></br><br></br>
+//                             <input type="email" placeholder='Your Email' name='Email' value={data.Email} onChange={form} style={{ width: '500px', height: '50px', borderRadius: '10px' }}></input><br></br><br></br>
+//                             <select style={{ width: '500px', height: '50px', borderRadius: '10px' }} name='Person' value={data.Person} onChange={form}>
+//                                 <option value="How many persons?">How many persons?</option>
+//                                 <option value="2">2</option>
+//                                 <option value="3">3</option>
+//                                 <option value="4">4</option>
+//                                 <option value="5">5</option>
+//                             </select><br></br><br></br>
+//                             <input type="date" name='Date' value={data.Date} onChange={form} style={{ width: '500px', height: '50px', borderRadius: '10px' }}></input>
+//                             <div className='pt-3'>
+//                                 <button className='btn btn-warning ps-5 pe-5' type="submit" value='submit' style={{ borderRadius: '20px', padding: '10px' }}>Book Now</button>
+//                             </div>
+//                         </form>
+//                     </div>
+//                     <div className='col-lg-6'>
+//                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15703.579908186117!2d76.39161387062377!3d10.270045393129344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0801fada2d85d7%3A0xb2c66280cc7f3e67!2sEat%20%26%20Meet%20Bakery!5e0!3m2!1sen!2sin!4v1755691793968!5m2!1sen!2sin" width="550" height="350" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" style={{ borderRadius: '20px' }}></iframe>
+//                     </div>
+//                 </div>
+//             </div>
+
+//         </div>
+//     )
+// }
+
+
+import './CSS/Home.css';
 import React, { useState } from 'react';
 import axios from 'axios';
-export default function Book() {
 
-    const [data, setdata] = useState({
+export default function Book() {
+    const [data, setData] = useState({
         Name: "",
         Ph_Number: "",
         Email: "",
@@ -11,54 +77,105 @@ export default function Book() {
         Date: ""
     });
 
-    const form = (e) => {
-        setdata({
+    const handleChange = (e) => {
+        setData({
             ...data,
             [e.target.name]: e.target.value
         });
     };
 
-    const submit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         axios.post("https://6888a259adf0e59551bac01f.mockapi.io/api/intern/Contact", data)
+            .then(() => alert("Booking submitted!"))
+            .catch((err) => console.error(err));
 
-        setdata({
+        setData({
             Name: "",
             Ph_Number: "",
             Email: "",
             Person: "",
             Date: ""
-        })
-    }
+        });
+    };
+
     return (
-        <div>
-            <div className='container pt-5 mt-5 pb-5'>
-                <h1>Book your Table</h1>
-                <div className='row pt-4'>
-                    <div className='col-lg-6'>
-                        <form onSubmit={submit}>
-                            <input type="text" placeholder='Your Name' name="Name" value={data.Name} onChange={form} style={{ width: '500px', height: '50px', borderRadius: '10px' }}></input><br></br><br></br>
-                            <input type="number" placeholder='Phone Number' name='Ph_Number' value={data.Ph_Number} onChange={form} style={{ width: '500px', height: '50px', borderRadius: '10px' }}></input><br></br><br></br>
-                            <input type="email" placeholder='Your Email' name='Email' value={data.Email} onChange={form} style={{ width: '500px', height: '50px', borderRadius: '10px' }}></input><br></br><br></br>
-                            <select style={{ width: '500px', height: '50px', borderRadius: '10px' }} name='Person' value={data.Person} onChange={form}>
-                                <option value="How many persons?">How many persons?</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select><br></br><br></br>
-                            <input type="date" name='Date' value={data.Date} onChange={form} style={{ width: '500px', height: '50px', borderRadius: '10px' }}></input>
-                            <div className='pt-3'>
-                                <button className='btn btn-warning ps-5 pe-5' type="submit" value='submit' style={{ borderRadius: '20px', padding: '10px' }}>Book Now</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div className='col-lg-6'>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15703.579908186117!2d76.39161387062377!3d10.270045393129344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0801fada2d85d7%3A0xb2c66280cc7f3e67!2sEat%20%26%20Meet%20Bakery!5e0!3m2!1sen!2sin!4v1755691793968!5m2!1sen!2sin" width="550" height="350" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" style={{ borderRadius: '20px' }}></iframe>
+        <div className="container pt-5 mt-5 pb-5">
+            <h1 className="text-center mb-5">Book Your Table</h1>
+            <div className="row g-4">
+                {/* Form Section */}
+                <div className="col-lg-6">
+                    <form onSubmit={handleSubmit} className="d-flex flex-column gap-3">
+                        <input 
+                            type="text" 
+                            placeholder='Your Name' 
+                            name="Name" 
+                            value={data.Name} 
+                            onChange={handleChange} 
+                            className="form-control form-control-lg"
+                            required 
+                        />
+                        <input 
+                            type="number" 
+                            placeholder='Phone Number' 
+                            name='Ph_Number' 
+                            value={data.Ph_Number} 
+                            onChange={handleChange} 
+                            className="form-control form-control-lg"
+                            required 
+                        />
+                        <input 
+                            type="email" 
+                            placeholder='Your Email' 
+                            name='Email' 
+                            value={data.Email} 
+                            onChange={handleChange} 
+                            className="form-control form-control-lg"
+                            required 
+                        />
+                        <select 
+                            name='Person' 
+                            value={data.Person} 
+                            onChange={handleChange} 
+                            className="form-select form-select-lg"
+                            required
+                        >
+                            <option value="">How many persons?</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                        <input 
+                            type="date" 
+                            name='Date' 
+                            value={data.Date} 
+                            onChange={handleChange} 
+                            className="form-control form-control-lg"
+                            required
+                        />
+                        <button 
+                            className='btn btn-warning btn-lg mt-3' 
+                            type="submit"
+                        >
+                            Book Now
+                        </button>
+                    </form>
+                </div>
+
+                {/* Map Section */}
+                <div className="col-lg-6">
+                    <div className="ratio ratio-16x9 rounded">
+                        <iframe 
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15703.579908186117!2d76.39161387062377!3d10.270045393129344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0801fada2d85d7%3A0xb2c66280cc7f3e67!2sEat%20%26%20Meet%20Bakery!5e0!3m2!1sen!2sin!4v1755691793968!5m2!1sen!2sin" 
+                            allowFullScreen="" 
+                            loading="lazy" 
+                            referrerPolicy="no-referrer-when-downgrade"
+                            title="Location Map"
+                        ></iframe>
                     </div>
                 </div>
             </div>
-
         </div>
-    )
+    );
 }
--- src/Project/Bookings.js
+++ src/Project/Bookings.js
@@ -1,59 +1,121 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// export default function Bookings() {
+//     const [userData, setUserData] = useState([]);
+
+//     useEffect(() => {
+//         axios.get("https://6888a259adf0e59551bac01f.mockapi.io/api/intern/Contact")
+//             .then(res => setUserData(res.data));
+//     }, []);
+
+//     const Delete = (id) => {
+//     if (window.confirm("Are you sure you want to delete this contact?")) {
+//         axios.delete(`https://6888a259adf0e59551bac01f.mockapi.io/api/intern/Contact/${id}`)
+//             .then(() => {
+//                 setUserData(prevData => prevData.filter(user => user.id !== id));
+//             })
+//     }
+// };
+
+
+//     return (
+//         <div style={{ padding: '20px' }}>
+//             <center>
+//                 <h2 className='pb-4'>Contact Details</h2>
+//                 {userData.length > 0 ? (
+//                     <table border="1" cellPadding="10" style={{ width: '80%', backgroundColor: 'white', boxShadow: '0 0 10px black' }}>
+//                         <thead>
+//                             <tr style={{ backgroundColor: 'green', color: 'white' }}>
+//                                 <th style={{ border: '2px solid black' }}>S.No</th>
+//                                 <th style={{ border: '2px solid black' }}>Name</th>
+//                                 <th style={{ border: '2px solid black' }}>Phone Number</th>
+//                                 <th style={{ border: '2px solid black' }}>Email</th>
+//                                 <th style={{ border: '2px solid black' }}>Person</th>
+//                                 <th style={{ border: '2px solid black' }}>Date</th>
+//                                 <th style={{ border: '2px solid black' }}>Delete</th>
+//                             </tr>
+//                         </thead>
+//                         <tbody>
+//                             {userData.map((user, index) => (
+//                                 <tr key={user.id} style={{ textAlign: 'left' }}>
+//                                     <td style={{ border: '2px solid black' }}>{index + 1}</td>
+//                                     <td style={{ border: '2px solid black' }}>{user.Name}</td>
+//                                     <td style={{ border: '2px solid black' }}>{user.Ph_Number}</td>
+//                                     <td style={{ border: '2px solid black' }}>{user.Email}</td>
+//                                     <td style={{ border: '2px solid black' }}>{user.Person}</td>
+//                                     <td style={{ border: '2px solid black' }}>{user.Date}</td>
+//                                     <td style={{ border: '2px solid black' }}><input type='button' className='btn btn-danger' value='Delete' onClick={() => Delete(user.id)}></input></td>
+//                                 </tr>
+//                             ))}
+//                         </tbody>
+//                     </table>
+//                 ) : (
+//                     <p>Loading data...</p>
+//                 )}
+//             </center>
+//         </div>
+//     );
+// }
+
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function Bookings() {
-    const [userData, setUserData] = useState([]);
+  const [userData, setUserData] = useState([]);
 
-    useEffect(() => {
-        axios.get("https://6888a259adf0e59551bac01f.mockapi.io/api/intern/Contact")
-            .then(res => setUserData(res.data));
-    }, []);
+  useEffect(() => {
+    axios.get("https://6888a259adf0e59551bac01f.mockapi.io/api/intern/Contact")
+      .then(res => setUserData(res.data));
+  }, []);
 
-    const Delete = (id) => {
+  const handleDelete = (id) => {
     if (window.confirm("Are you sure you want to delete this contact?")) {
-        axios.delete(`https://6888a259adf0e59551bac01f.mockapi.io/api/intern/Contact/${id}`)
-            .then(() => {
-                setUserData(prevData => prevData.filter(user => user.id !== id));
-            })
+      axios.delete(`https://6888a259adf0e59551bac01f.mockapi.io/api/intern/Contact/${id}`)
+        .then(() => {
+          setUserData(prevData => prevData.filter(user => user.id !== id));
+        });
     }
-};
+  };
 
-
-    return (
-        <div style={{ padding: '20px' }}>
-            <center>
-                <h2 className='pb-4'>Contact Details</h2>
-                {userData.length > 0 ? (
-                    <table border="1" cellPadding="10" style={{ width: '80%', backgroundColor: 'white', boxShadow: '0 0 10px black' }}>
-                        <thead>
-                            <tr style={{ backgroundColor: 'green', color: 'white' }}>
-                                <th style={{ border: '2px solid black' }}>S.No</th>
-                                <th style={{ border: '2px solid black' }}>Name</th>
-                                <th style={{ border: '2px solid black' }}>Phone Number</th>
-                                <th style={{ border: '2px solid black' }}>Email</th>
-                                <th style={{ border: '2px solid black' }}>Person</th>
-                                <th style={{ border: '2px solid black' }}>Date</th>
-                                <th style={{ border: '2px solid black' }}>Delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {userData.map((user, index) => (
-                                <tr key={user.id} style={{ textAlign: 'left' }}>
-                                    <td style={{ border: '2px solid black' }}>{index + 1}</td>
-                                    <td style={{ border: '2px solid black' }}>{user.Name}</td>
-                                    <td style={{ border: '2px solid black' }}>{user.Ph_Number}</td>
-                                    <td style={{ border: '2px solid black' }}>{user.Email}</td>
-                                    <td style={{ border: '2px solid black' }}>{user.Person}</td>
-                                    <td style={{ border: '2px solid black' }}>{user.Date}</td>
-                                    <td style={{ border: '2px solid black' }}><input type='button' className='btn btn-danger' value='Delete' onClick={() => Delete(user.id)}></input></td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                ) : (
-                    <p>Loading data...</p>
-                )}
-            </center>
+  return (
+    <div className="container py-4">
+      <h2 className="text-center mb-4">Contact Details</h2>
+      {userData.length > 0 ? (
+        <div className="table-responsive shadow-sm bg-white p-3 rounded">
+          <table className="table table-bordered table-hover">
+            <thead className="table-success text-center">
+              <tr>
+                <th>S.No</th>
+                <th>Name</th>
+                <th>Phone Number</th>
+                <th>Email</th>
+                <th>Person</th>
+                <th>Date</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {userData.map((user, index) => (
+                <tr key={user.id}>
+                  <td className="text-center">{index + 1}</td>
+                  <td>{user.Name}</td>
+                  <td>{user.Ph_Number}</td>
+                  <td>{user.Email}</td>
+                  <td>{user.Person}</td>
+                  <td>{user.Date}</td>
+                  <td className="text-center">
+                    <button className="btn btn-danger btn-sm" onClick={() => handleDelete(user.id)}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      ) : (
+        <p className="text-center">Loading data...</p>
+      )}
+    </div>
+  );
 }
